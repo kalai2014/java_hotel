09adbfc3c34e985461828935fd9aee540caa7b45
Kalainidhi T <kalainidhi1999@gmail.com>
2023-12-28 18:41:07 -0600

    Printed all the options for hotel booking

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello and welcome!");
-
+        System.out.println("Hotel Booking Management");
+        System.out.println("1. Register Account");
+        System.out.println("2. Book Reservation");
+        System.out.println("3. Generate Bill");
+        System.out.println("4. Feedback");
 
     }
 }
